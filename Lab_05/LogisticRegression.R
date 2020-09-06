ffab3b0d6eb0a850c5164e29781c82908fb63bde
fruxc <hammadansari17@gmail.com>
2020-09-06 22:44:12 +0530

    Lab_05

---
+++ Lab_05/LogisticRegression.R
@@ -0,0 +1,34 @@
+data(mtcars)
+dataset <- subset(mtcars, select=c(mpg, am, vs))
+dataset
+
+prob_vm <- glm(vs ~ mpg, dataset, family=binomial(link="logit"))
+
+summary(prob_vm)
+
+library(ggplot2)
+ggplot(dataset, aes(x=mpg, y=vs)) + geom_point() + 
+  stat_smooth(method="glm", method.args=list(family="binomial"), se=FALSE)
+
+par(mar = c(4, 4, 1, 1)) # Reduce some of the margins so that the plot fits better
+plot(dataset$mpg, dataset$vs)
+curve(predict(prob_vm, data.frame(mpg=x), type="response"), add=TRUE) 
+
+
+prob_va <- glm(vs ~ am, dataset, family=binomial)
+summary(prob_va)
+
+
+ggplot(dataset, aes(x=am, y=vs)) + 
+  geom_point(shape=1, position=position_jitter(width=.05,height=.05)) + 
+  stat_smooth(method="glm", method.args=list(family="binomial"), se=FALSE)
+
+
+par(mar = c(4, 4, 1, 1)) # Reduce some of the margins so that the plot fits better
+plot(jitter(dataset$am, .2), jitter(dataset$vs, .2))
+curve(predict(prob_va, data.frame(am=x), type="response"), add=TRUE) 
+
+
+prob_vma <- glm(vs ~ mpg + am, dataset, family=binomial)
+prob_vma
+summary(prob_vma)
---
+++ Lab_05/LogisticRegressionOnMtcars.R
@@ -0,0 +1,33 @@
+#Loading the data
+library(caTools)
+write.csv(mtcars, file = "mtcars.csv")
+myData <- read.csv("mtcars.csv", header = T)
+
+#Splitting the data into training and testing
+split <- sample.split(myData, SplitRatio = 0.8)
+split 
+train <- subset(myData, split=="TRUE")
+test <- subset(myData, split=="FALSE")
+
+
+#Train the model using entire data
+myModel <- glm(am ~ wt+disp, data = myData, family="binomial")
+summary(myModel)
+ggplot(myData, aes(x=wt+disp, y=am)) + geom_point() + 
+  stat_smooth(method="glm", method.args=list(family="binomial"), se=FALSE)
+
+newData = data.frame(disp=120, wt=2.8)
+predict(myModel, newData, type="response") 
+
+#Train the model using training data
+myModel <- glm(am ~ wt+disp, data = train, family="binomial")
+summary(myModel)
+
+#Running the test data through the model
+
+res <- predict(myModel, test, type="response")
+res
+res <- predict(myModel, train, type="response")
+res
+
+
