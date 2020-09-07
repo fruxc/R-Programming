6f2584d089096923a6876772d035fd18328e51a6
fruxc <hammadansari17@gmail.com>
2020-09-07 17:08:44 +0530

    Lab 06

---
+++ Lab_06/NaiveBayesIris.R
@@ -0,0 +1,19 @@
+#loading the data and libraries
+library(e1071)
+library(caTools)
+
+data(iris)
+
+#splitting the data for training and testing purpose
+
+split <- sample.split(iris, SplitRatio=0.7)
+train <- subset(iris, split=TRUE)
+test <- subset(iris, split=FALSE)
+
+#creating naive bayes model
+
+myModel <- naiveBayes(train[,1:4], train[,5]) 
+
+table(predict(myModel, test[,-5]), test[,5], dnn=list('Predicted','Actual')) #returns the confusion matrix
+
+myModel$apriori
