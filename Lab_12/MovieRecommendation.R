c8042bfc677cc90b3221eac1edc0aaaae2ff551a
Hammad Ansari <hammadansari17@gmail.com>
2020-11-04 10:26:41 +0530

    Lab 12

--- Lab_12/MovieRecommendation.R
+++ Lab_12/MovieRecommendation.R
@@ -21,14 +21,14 @@ class(MovieLense@data)
 head(names(colCounts(MovieLense)))
 
 vector_ratings <- as.vector(MovieLense@data)
-kable(table(vector_ratings), caption="Rating frequency")
+kable(table(vector_ratings), caption = "Rating frequency")
 
 
-# Since a rating of 0 represents absence of a rating in this data set, we can remove such
+# Since a rating of 0 represents the absence of a rating in this data set, we can remove such
 # ratings from the ratings vector.
 
 vector_ratings = vector_ratings[vector_ratings != 0]
-hist(vector_ratings, main="Histogram of Ratings", xlab="Rating Value")
+hist(vector_ratings, main = "Histogram of Ratings", xlab = "Rating Value")
 
 # Data Preparation
 
@@ -42,15 +42,15 @@ dim(ratings)
 ratings.n = normalize(ratings)
 ratings.n.vec = as.vector(ratings.n@data)
 ratings.n.vec = ratings.n.vec[ratings.n.vec != 0]
-hist(ratings.n.vec, main="Histogram of Normalized Ratings", xlab="Rating")
+hist(ratings.n.vec, main = "Histogram of Normalized Ratings", xlab = "Rating")
 
 # Splitting data for test and train
 
 percent_train = 0.8
 #min(rowCounts(ratings.n))
-items_to_keep = 15        # items to use for each user
-rating_threshold = 3      # good rating implies >=3
-n_eval = 1                # number of times to run eval
+items_to_keep = 15 # items to use for each user
+rating_threshold = 3 # good rating implies >=3
+n_eval = 1 # number of times to run eval
 
 eval_sets = evaluationScheme(data = ratings, method = "split",
                              train = percent_train, given = items_to_keep,
@@ -94,7 +94,7 @@ models_to_evaluate = list(IBCF_cos = list(name = "IBCF", param = list(method = "
                           IBCF_cor = list(name = "IBCF", param = list(method = "pearson")),
                           UBCF_cos = list(name = "UBCF", param = list(method = "cosine")),
                           UBCF_cor = list(name = "UBCF", param = list(method = "pearson")),
-                          random = list(name = "RANDOM", param=NULL))
+                          random = list(name = "RANDOM", param = NULL))
 
 n_recommendations = c(1, 3, 5, 10, 15, 20)
 results = evaluate(x = eval_sets, method = models_to_evaluate, n = n_recommendations)
@@ -102,11 +102,11 @@ results = evaluate(x = eval_sets, method = models_to_evaluate, n = n_recommendat
 # Comparing the Collaborative Filtering Models
 
 # Draw ROC curve
-plot(results, y = "ROC", annotate = 1, legend="topleft")
+plot(results, y = "ROC", annotate = 1, legend = "topleft")
 title("ROC Curve")
 
 
 # Draw precision / recall curve
-plot(results, y = "prec/rec", annotate=1)
+plot(results, y = "prec/rec", annotate = 1)
 title("Precision-Recall")
 
